8f168bee9995d8010768531befdf77f6a4e91285
Ryan Ledford <rledford@users.noreply.github.com>
2021-04-07 10:31:09 -0500

    updated tests

--- mongoqs_test.go
+++ mongoqs_test.go
@@ -11,7 +11,7 @@ func TestNewQProcessor(t *testing.T) {
 	myStringField := NewQField("myString", QString)
 	myIntField := NewQField("myInt", QInt)
 	myIntField.IsSortable() // allow this field to be used in sorts
-	myIntField.IsProjectable() // // allow this field to be used in projections
+	myIntField.IsProjectable() // allow this field to be used in projections
 	myFloatField := NewQField("myFloat", QFloat)
 	myFloatField.IsSortable().IsProjectable() // same as calls on myIntField but chained
 	myBoolField := NewQField("myBool", QBool)
@@ -21,15 +21,15 @@ func TestNewQProcessor(t *testing.T) {
 	// create a new query processor
 	qproc := NewQProcessor(myStringField, myIntField, myFloatField, myBoolField, myDateTimeField, myObjectIDField)
 
-	// we'll use the net/url package's Values to construct query to process, but it would be more common to use on from an http request
+	// we'll use the net/url package's Values to construct query to process, but it would be more common to use one from an http request
 	qs := url.Values{}
 	qs.Add("unknown", "nin:1,2,3,4") // a QField was not created for 'unknown' so it will be ignored
-	qs.Add("myString", "Hello, world")
+	qs.Add("myString", "like:Hello, world")
 	qs.Add("myInt", "gt:1,lt:10")
 	qs.Add("myFloat", "1.0") // 'eq:' operator is assumed
 	qs.Add("myBool", "false") // 'eq:' operator is assumed
-	qs.Add("myDateTime", "gte:2021-01-01T15:00:00Z")
-	qs.Add("id", "6050e7f529a90b22dc47f19e") // using an alias of myObjectID
+	qs.Add("myDateTime", "gte:2021-01-01T15:00:00Z,lte:2021-02-01T15:00:00Z")
+	qs.Add("id", "in:6050e7f529a90b22dc47f19e,6050e7f529a90b22dc47f19f") // using an alias of myObjectID
 	qs.Add("srt", "-myInt,+myString") // sort by myInt in descending order (myStringField.IsSortable() was not called so +myString will be ignored)
 	qs.Add("prj", "-myFloat") // exclude myFloat field from query results
 	qs.Add("lmt", "10") // limit to 10 results
