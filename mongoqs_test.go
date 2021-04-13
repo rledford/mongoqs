792c736ca6e3404f98d01afd74b0eef46e73c56c
Ryan Ledford <rledford@users.noreply.github.com>
2021-04-13 13:19:51 -0500

    updated test to include Meta output

--- mongoqs_test.go
+++ mongoqs_test.go
@@ -8,18 +8,24 @@ import (
 
 func TestNewQProcessor(t *testing.T) {
 	// create and configure query fields
-	myStringField := NewQField("myString", QString)
-	myIntField := NewQField("myInt", QInt)
+	myStringField := NewQField("myString")
+	myIntField := NewQField("myInt")
+	myIntField.ParseAsInt() // parse query string values integers
 	myIntField.Sortable() // allow this field to be used in sorts
 	myIntField.Projectable() // allow this field to be used in projections
-	myFloatField := NewQField("myFloat", QFloat)
-	myFloatField.Sortable().Projectable() // same as calls on myIntField but chained
-	myBoolField := NewQField("myBool", QBool)
-	myDateTimeField := NewQField("myDateTime", QDateTime)
-	myObjectIDField := NewQField("myObjectID", QObjectID)
+	myFloatField := NewQField("myFloat")
+	myFloatField.ParseAsFloat().Sortable().Projectable() // same as calls on myIntField but chained
+	myBoolField := NewQField("myBool")
+	myBoolField.ParseAsBool()
+	myDateTimeField := NewQField("myDateTime")
+	myDateTimeField.ParseAsDateTime()
+	myObjectIDField := NewQField("myObjectID")
 	myObjectIDField.UseAliases("_id", "id") // will use _id and id to refer to myObjectID
+	myObjectIDField.ParseAsObjectID()
+	myMetaField := NewQField("pageMarker")
+	myMetaField.ParseAsMeta()
 	// create a new query processor
-	qproc := NewQProcessor(myStringField, myIntField, myFloatField, myBoolField, myDateTimeField, myObjectIDField)
+	qproc := NewQProcessor(myStringField, myIntField, myFloatField, myBoolField, myDateTimeField, myObjectIDField, myMetaField)
 
 	// we'll use the net/url package's Values to construct query to process, but it would be more common to use one from an http request
 	qs := url.Values{}
@@ -30,6 +36,7 @@ func TestNewQProcessor(t *testing.T) {
 	qs.Add("myBool", "false") // 'eq:' operator is assumed
 	qs.Add("myDateTime", "gte:2021-01-01T15:00:00Z,lte:2021-02-01T15:00:00Z")
 	qs.Add("id", "in:6050e7f529a90b22dc47f19e,6050e7f529a90b22dc47f19f") // using an alias of myObjectID
+	qs.Add("pageMarker", "6050e7f529a90b22dc47f19f")
 	qs.Add("srt", "-myInt,+myString") // sort by myInt in descending order (myStringField.Sortable() was not called so +myString will be ignored)
 	qs.Add("prj", "-myFloat") // exclude myFloat field from query results
 	qs.Add("lmt", "10") // limit to 10 results
